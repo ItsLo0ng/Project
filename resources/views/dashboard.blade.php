0300a233d5d1f4b66c68cf814eabd7fd6c06bddf
ItsLo0ng <oxygennot@gmail.com>
2026-01-25 02:47:28 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,20 +1,22 @@
+@section('content')
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Dashboard jejeje') }}
         </h2>
     </x-slot>
-
-    <div class="py-12">
-        @if (auth()->user()->role === 'admin')
-            <a href="/admin" class="btn btn-primary">Go to Admin Panel</a>
-        @endif
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+    @section('content')
+        <div class="py-12">
+            @if (auth()->user()->role === 'admin')
+                <a href="/admin" class="btn btn-primary">Go to Admin Panel</a>
+            @endif
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        {{ __("You're logged in!") }}
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    @endsection
 </x-app-layout>
